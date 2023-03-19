bb0c928af807be904227896e1e92d1cce78a345a
Ivan Zavaleta <ivzavaleta@gmail.com>
2023-03-19 16:18:07 -0500

    created javascript file, defined search button and table body, need to psuedo code logic and how we will fill the tables with data value

---
+++ assets/index.js
@@ -0,0 +1,8 @@
+var tableBody = document.getElementById('movieResultsTable')
+var searchButton = document.getElementById('searchButton')
+
+function getApi() {
+  var 
+}
+
+searchButton.addEventListener('click', getApi)
