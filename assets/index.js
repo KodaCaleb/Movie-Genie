50213280238a97c90bf54f6df6b0256227710dd1
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-03-23 20:21:13 -0500

    fixed buttons and selection queries in logic to only have 1 button selected

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,6 @@
 // API key and host for the movies database & streaming availability
 const apiKey = '56b9500ae6msh95c67a302f2b6e8p1c367fjsna89523e75e53';
 const apiHost = 'moviesdatabase.p.rapidapi.com';
-//const apiKey2 = '56b9500ae6msh95c67a302f2b6e8p1c367fjsna89523e75e53';
 const apiHost2 = 'streaming-availability.p.rapidapi.com';
 
 // References to the HTML elements
@@ -12,35 +11,44 @@ const sortButtons = document.querySelectorAll('.sort-button');
 
 // Array to store the selected genres
 let selectedGenres = [];
-// String to store the selected sort 
+// String to store the selected sort
 let selectedSort = '';
 
 // Add a click event listener to each genre button
 genreButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Toggle the 'selected' class on the button
-        button.classList.toggle('selected');
+        // Remove the 'selected' class from all genre buttons
+        genreButtons.forEach((otherButton) => {
+            otherButton.classList.remove('selected');
+        });
+
+        // Add the 'selected' class to the clicked button
+        button.classList.add('selected');
+
         const genre = button.textContent;
 
-        // Add or remove the genre from the selectedGenres array
-        if (selectedGenres.includes(genre)) {
-            selectedGenres = selectedGenres.filter((g) => g !== genre);
-        } else {
-            selectedGenres.push(genre);
-        }
+        // Set the selectedGenres array to contain only the clicked genre
+        selectedGenres = [genre];
     });
 });
-//event listener for sort buttons
+
+// Event listener for sort buttons
 sortButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Toggle the 'selected' class on the button
-        button.classList.toggle('selected');
-        // give string value
-        let selectedSort = button.textContent;
+        // Remove the 'selected' class from all sort buttons
+        sortButtons.forEach((otherButton) => {
+            otherButton.classList.remove('selected');
+        });
 
-        console.log(selectedSort)
+        // Add the 'selected' class to the clicked button
+        button.classList.add('selected');
 
-        // check string value/replace with url string
+        // Set the selectedSort string value
+        selectedSort = button.textContent;
+
+        console.log(selectedSort);
+
+        // Replace the selectedSort string value with the corresponding URL string
         if (selectedSort.includes('Popular Movies')) {
             selectedSort = 'most_pop_movies';
         }
@@ -54,34 +62,25 @@ sortButtons.forEach((button) => {
             selectedSort = 'top_rated_lowest_100';
         }
 
-        console.log(selectedSort)
-
-        // Add or remove the genre from the selectedGenres array
-        // if (selectedSort.includes(listSort)) {
-        //     selectedSort = selectedListSort.filter((g) => g !== listSort);
-        // } else {
-        //     selectedSort.push(listSort);
-        // }
+        console.log(selectedSort);
     });
 });
 
 // Add a click event listener to the search button
 searchButton.addEventListener('click', async () => {
-    
+
     // If no genres are selected, alert the user and return early
-    if (selectedGenres.length === 0 || selectedGenres.length > 1) {
+    if (selectedGenres.length === 0) {
         alert('Please select one genre.');
         return;
     }
 
     // Construct the genre query string
     const genreQuery = selectedGenres.map((genre) => `&genre=${encodeURIComponent(genre)}`).join('');
-    // Construct the list sort query string
-    //const listSortQuery = selectedSort.map((listSort) => `&list=${encodedURIComponent(listSort)}`).join('');
     const sortQuery = '&list=' + selectedSort;
     // Create the Movie Database API URL with query parameters
     const apiUrl = `https://moviesdatabase.p.rapidapi.com/titles?titleType=movie${genreQuery}${sortQuery}`;
-        // 
+
     // Define options for the API request
     const options = {
         method: 'GET',
@@ -97,46 +96,38 @@ searchButton.addEventListener('click', async () => {
         const response = await fetch(apiUrl, options);
         const data = await response.json();
 
-        console.log(data.results)
+        console.log(data.results);
+                // Extract movie results from the data object
+                const movies = data.results;
 
-        // Extract movie results from the data object
-        const movies = data.results;
-
-        // Clear the list container's inner HTML
-        listContainer.innerHTML = '';
-
-        // Iterate through the movie results
-        movies.forEach((movie) => {
-            // Create a movie card element
-            const movieCard = document.createElement('div');
-            movieCard.classList.add('list-el', 'callout', 'secondary');
-
-            // Create and set the movie poster element
-            // const poster = document.createElement('img');
-            // poster.classList.add('movie-poster');
-            // poster.src = movie.primaryImage.url;
-            // poster.alt = movie.titleText.text;
-
-            // Create and set the movie title element
-            const title = document.createElement('h3');
-            title.classList.add('movie-title');
-            title.textContent = movie.titleText.text;
-
-            // Create and set the movie description element
-            const description = document.createElement('p');
-            description.classList.add('movie-description');
-            description.textContent = `Release Year: ${movie.releaseYear.year}`;
-
-            // Append the poster, title, and description to the movie card
-//            movieCard.appendChild(poster);
-            movieCard.appendChild(title);
-            movieCard.appendChild(description);
-
-            // Append the movie card to the list container
-            listContainer.appendChild(movieCard);
+                // Clear the list container's inner HTML
+                listContainer.innerHTML = '';
+        
+                // Iterate through the movie results
+                movies.forEach((movie) => {
+                    // Create a movie card element
+                    const movieCard = document.createElement('div');
+                    movieCard.classList.add('list-el', 'callout', 'secondary');
+        
+                    // Create and set the movie title element
+                    const title = document.createElement('h3');
+                    title.classList.add('movie-title');
+                    title.textContent = movie.titleText.text;
+        
+                    // Create and set the movie description element
+                    const description = document.createElement('p');
+                    description.classList.add('movie-description');
+                    description.textContent = `Release Year: ${movie.releaseYear.year}`;
+        
+                    // Append the title and description to the movie card
+                    movieCard.appendChild(title);
+                    movieCard.appendChild(description);
+        
+                    // Append the movie card to the list container
+                    listContainer.appendChild(movieCard);
+                });
+            } catch (err) {
+                // Log any errors to the console
+                console.error(err);
+            }
         });
-    } catch (err) {
-        // Log any errors to the console
-        console.error(err);
-    }
-});
